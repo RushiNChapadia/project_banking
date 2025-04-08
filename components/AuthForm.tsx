4722c5ac5a9ad0b454e6da9b373604ab21ec62ee
RushiNChapadia <rushichapadia05@gmail.com>
2025-04-08 16:36:07 -0700

    Implemented PlaidLink

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,6 +24,7 @@ import { Loader2 } from 'lucide-react';
 import SignUp from '@/app/(auth)/sign-up/page';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.action';
+import PlaidLink from './PlaidLink';
 
 
 const AuthForm = ({ type }: {type: string }) => {
@@ -51,9 +52,23 @@ const AuthForm = ({ type }: {type: string }) => {
           
           try {
             // sign up with appwrite & create plaid token
-
+            
+            
             if(type === 'sign-up') {
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+
+                const newUser = await signUp(userData);
 
                 setUser(newUser);
             }
@@ -103,11 +118,11 @@ const AuthForm = ({ type }: {type: string }) => {
                 </p>
             </h1>
         </header>
-        {user ? (
+        {/* {user ? ( */}
             <div className='flex flex-col gap-4'>
-                {/* PlaidLink */}
+                <PlaidLink user={user} variant='primary' />
             </div>
-        ): (
+        {/* ): ( */}
             <>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -182,7 +197,7 @@ const AuthForm = ({ type }: {type: string }) => {
                 </Link>
             </footer>
             </>
-        )}
+        {/* )} */}
     </section>
   )
 }
